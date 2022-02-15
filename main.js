3697246bd303063ef325babf154507a536a85644
Venomous2010 <79148708+Venomous2010@users.noreply.github.com>
2022-02-15 17:29:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+video = "";
+objects = [];
+Status = ""
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(430, 330);
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(Status != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status : objects detected";
+            document.getElementById("number_of_objects").innerHTML = "number_of_objects detected =" + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
